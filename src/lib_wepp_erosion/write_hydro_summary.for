a19c7b6aa5289a4ff88de7fa00031a957d2fcc71
Fred Fox <fred.fox@usda.gov>
2016-11-01 22:40:39 +0000

    Manually merged trunk changes commit 14616
    Changed continuation format to work with intel compiler
    
    git-svn-id: https://infosys.ars.usda.gov/svn/code/weps1/branches/weps.src.subregion@14617 77af8a48-1994-4e4a-b6f4-b1c89642997b

--- src/lib_wepp_erosion/write_hydro_summary.for
+++ src/lib_wepp_erosion/write_hydro_summary.for
@@ -23,30 +23,27 @@
       return 
       
  1050 format(//'AVERAGE ANNUAL SUMMARIES',/,72('-'))
- 1200 format (//'I.   RAINFALL AND RUNOFF SUMMARY',/,5x,8('-'),1x,3('-'
-     1    ),1x,6('-'),1x,7('-'),//,6x,'total summary: ',' years ',i4,
-     1    ' - ',i4)
- 1350 format(/5x,i5,
-     1    ' storms produced                       ',f9.2,
-     1    ' mm of precipitation',/,5x,i5,
-     1    ' rain storm runoff events produced     ',f9.2,
-     1    ' mm of runoff',/,5x,i5,
-     1    ' snow melts and/or',/,10x,
-     1    '   events during winter produced       ',f9.2,
-     1    ' mm of runoff',/)  
- 1650 format (6x,'annual averages'/6x,'---------------'//6x,
-     1    '  Number of years                              ',
-     1    3x,i4,/,6x,
-     1    '  Mean annual precipitation                    ',
-     1    f7.2,1x,'mm',/,6x,
-     1    '  Mean annual runoff from rainfall             ',
-     1    f7.2,1x,'mm',/,6x,
-     1    '  Mean annual runoff from snow melt',/,6x,
-     1    '    and/or rain storm during winter            ',
-     1    f7.2,1x,'mm',/)   
+ 1200 format (//'I.   RAINFALL AND RUNOFF SUMMARY',/,5x,8('-'),1x,3('-' &
+     &    ),1x,6('-'),1x,7('-'),//,6x,'total summary: ',' years ',i4,   &
+     &    ' - ',i4)
+ 1350 format(/5x,i5,                                                    &
+     &    ' storms produced                       ',f9.2,               &
+     &    ' mm of precipitation',/,5x,i5,                               &
+     &    ' rain storm runoff events produced     ',f9.2,               &
+     &    ' mm of runoff',/,5x,i5,                                      &
+     &    ' snow melts and/or',/,10x,                                   &
+     &    '   events during winter produced       ',f9.2,               &
+     &    ' mm of runoff',/)  
+ 1650 format (6x,'annual averages'/6x,'---------------'//6x,            &
+     &    '  Number of years                              ',            &
+     &    3x,i4,/,6x,                                                   &
+     &    '  Mean annual precipitation                    ',            &
+     &    f7.2,1x,'mm',/,6x,                                            &
+     &    '  Mean annual runoff from rainfall             ',            &
+     &    f7.2,1x,'mm',/,6x,                                            &
+     &    '  Mean annual runoff from snow melt',/,6x,                   &
+     &    '    and/or rain storm during winter            ',            &
+     &    f7.2,1x,'mm',/)
       
       
       end
-      
-      
-      
